403b6e50b74c7f7135e84b40d4b09fa198d2fb5f
amitayben <amitayben@gmail.com>
2017-08-20 16:39:48 +0300

    Update open_minicom.sh

--- open_minicom.sh
+++ open_minicom.sh
@@ -57,7 +57,8 @@ rm tmp_script*.sh set_ip*.sh
 ##############################
 NUM_OF_PORTS="$(ls /dev/ttyUSB* | grep -v ^l | wc -l)"
 if [NUM_OF_PORTS < 1];than
-	echo "Not found any minicom ports\n Exiting......" && exit(0)
+	echo -e "Not found any minicom ports\n Exiting......"
+	exit 1;
 fi
 for (( i=0; i < $NUM_OF_PORTS ; i++ ))
 do
